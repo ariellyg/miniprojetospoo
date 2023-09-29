04994835b19d4670d63ff5987f63f6f3d705cf7b
arielly_g <ariellyg69@gmail.com>
2023-09-28 23:10:19 -0300

    atualizacao

--- carro-ecologico/src/carro.py
+++ carro-ecologico/src/carro.py
@@ -1,25 +1,52 @@
 class Carro:
 
     def __init__(self):
-        pass
+        self.tanque = 0
+        self.passageiros = 0
+        self.quilometragem = 0
 
     def getPassageiros(self):
-        return -1
+       return self.passageiros
 
     def getCombustivel(self):
-        return -1
+        return self.tanque
 
     def getQuilometragem(self):
-        return -1
+        return self.quilometragem
 
     def embarcar(self):
-        return False
+        if self.passageiros < 2:
+            self.passageiros += 1
+            return True
+        else:
+            return False
 
     def desembarcar(self):
-        return False
+        if self.passageiros <= 2 and self.passageiros != 0:
+            self.passageiros -= 1
+            return True
+        else:
+            return False
 
     def dirigir(self, distancia):
-        return False
-
+        if self.passageiros > 0 and self.tanque > 0:
+            consumo = distancia
+            if consumo <= self.tanque:
+                self.tanque -= consumo
+                self.quilometragem += distancia
+                return True
+            else:
+                self.quilometragem+= self.tanque
+                self.tanque = 0
+                return 'voce esta sem combustivel'
     def abastecer(self, quantidade):
-        return False
+        if self.tanque + quantidade < 100:
+            self.tanque += quantidade
+            return 'quantidade nao chegou ao limite'
+        if self.tanque + quantidade == 100:
+            self.tanque += quantidade
+            return 'valor max atingido'
+        else:
+            return quantidade-100
+
+
