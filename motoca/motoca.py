bf8ed2ab2e6110a6143ee9d67e312f848d190381
arielly_g <ariellyg69@gmail.com>
2023-09-29 02:27:15 -0300

    atualizacao

--- motoca/motoca.py
+++ motoca/motoca.py
@@ -4,8 +4,9 @@ from src.pessoa import Pessoa
 class Motoca:
 
     def __init__(self, potencia: int):
+        self.potencia = potencia
         self.minutos = 0
-        self.pessoas = 0
+        self.pessoas = None
 
     def getPessoa(self):
         return self.pessoas
@@ -14,19 +15,44 @@ class Motoca:
         return self.minutos
 
     def getPotencia(self):
-        return -1
+        return self.potencia
 
     def subir(self, pessoa: Pessoa):
-        return False
+        if self.pessoas is None :
+            self.pessoas = pessoa
+            return True
+        else:
+            return False
+
 
     def descer(self):
-        return False
+        if self.pessoas is not None:
+            self.pessoas = None
+            return True
+        else:
+            return False
 
     def colocarTempo(self, tempo: int):
-        return False
+        if tempo > 0:
+            self.minutos += tempo
+            return True
+        else:
+            return False
 
     def dirigir(self, tempo: int):
-        return True
-
+        if self.pessoas is not None and self.minutos >= tempo:
+            if self.pessoas.getIdade <= 10:
+                dirigiu = tempo
+            else:
+                dirigiu = 0
+                self.minutos -= tempo
+            return dirigiu
+        else:
+            return 0
     def buzinar(self):
-        return None
+        if self.pessoas is not None:
+            buzina = 'p'
+            for i in range(self.potencia):
+                buzina += 'e'
+                buzina += 'm'
+                return buzina
