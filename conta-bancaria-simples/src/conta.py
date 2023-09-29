1dd4bb682d077b9cd30a123aea40a5b73fb156f9
arielly_g <ariellyg69@gmail.com>
2023-09-29 17:27:02 -0300

    atualizacao

--- conta-bancaria-simples/src/conta.py
+++ conta-bancaria-simples/src/conta.py
@@ -1,24 +1,61 @@
 class Conta:
     def __init__(self, numero:int, saldo:float):
-        pass
+        self.limite= 100
+        self.extrato = []
+        self.saldo = saldo
+        self.numero = numero
+        self.operacoes = 0
 
     def getNumero(self):
-        return 0
+        return self.numero
 
     def getSaldo(self):
-        return 0.0
+        return self.saldo + self.limite
 
     def getLimite(self):
-        return 0.0
+        return self.limite
 
     def sacar(self, valor: float):
-        return False
+        if valor > 0:
+            if self.saldo >= valor:
+                self.saldo -=valor
+                self.extrato.append(-valor)
+            elif self.saldo + self.limite >= valor:
+                self.limite -= (valor- self.saldo)
+                self.saldo =0
+                self.extrato.append(-valor)
+            else:
+                return False
+            return True
+        else:
+            return False
 
     def depositar(self, valor: float):
-        return True
-
+        if valor > 0:
+            self.saldo += valor
+            self.extrato.append(valor)
+            self.operacoes +=1
+            if self.saldo > 0:
+                self.limite = 100
+            return True
+        else:
+            return False
     def transferir(self, destino, valor:float):
-        return False
+        if destino and valor > 0:
+            if self.saldo >= valor:
+                self.saldo -= valor
+            elif self.saldo + self.limite >= valor:
+                self.limite -= (valor - self.saldo)
+                self.saldo = 0
+                destino.depositar(-valor)
+            else:
+                return False
+            destino.depositar(valor)
+            self.extrato.append(-valor)
+            self.operacoes +=1
+            return True
+        else:
+            return False
 
     def verExtrato(self):
-        return None
+        return self.extrato[-10:]
