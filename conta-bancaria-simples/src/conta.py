19b2a1e89f3dcabdbacb553ca4d4d68a162964ab
arielly_g <ariellyg69@gmail.com>
2023-09-29 20:12:10 -0300

    atualizacao

--- conta-bancaria-simples/src/conta.py
+++ conta-bancaria-simples/src/conta.py
@@ -2,30 +2,25 @@ class Conta:
     def __init__(self, numero:int, saldo:float):
         self.limite= 100
         self.extrato = []
-        self.saldo = saldo
+        self.saldo = saldo + self.limite
         self.numero = numero
-        self.operacoes = 0
+
 
     def getNumero(self):
         return self.numero
 
     def getSaldo(self):
-        return self.saldo + self.limite
+        return self.saldo
 
     def getLimite(self):
         return self.limite
 
     def sacar(self, valor: float):
-        if valor > 0:
-            if self.saldo >= valor:
-                self.saldo -=valor
-                self.extrato.append(-valor)
-            elif self.saldo + self.limite >= valor:
-                self.limite -= (valor- self.saldo)
-                self.saldo =0
-                self.extrato.append(-valor)
-            else:
-                return False
+        if valor > self.saldo - self.limite:
+            self.limite = self.saldo - valor
+        if 0 < valor <= self.saldo:
+            self.saldo -= valor
+            self.extrato.append(-valor)
             return True
         else:
             return False
@@ -34,28 +29,21 @@ class Conta:
         if valor > 0:
             self.saldo += valor
             self.extrato.append(valor)
-            self.operacoes +=1
-            if self.saldo > 0:
+            if self.limite == 0:
+                self.limite += valor
+            if self.saldo >= 100:
                 self.limite = 100
             return True
         else:
             return False
     def transferir(self, destino, valor:float):
-        if destino and valor > 0:
-            if self.saldo >= valor:
-                self.saldo -= valor
-            elif self.saldo + self.limite >= valor:
-                self.limite -= (valor - self.saldo)
-                self.saldo = 0
-                destino.depositar(-valor)
+        if destino:
+            if 0 < valor <= self.saldo:
+                self.sacar(valor)
+                destino.depositar(valor)
+                return True
             else:
                 return False
-            destino.depositar(valor)
-            self.extrato.append(-valor)
-            self.operacoes +=1
-            return True
-        else:
-            return False
 
     def verExtrato(self):
-        return self.extrato[-10:]
+        return self.extrato
